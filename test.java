0ae03a5dfa06675743e2f66c08a0d694e9c7c37b
MAYUKH GHOSH <93669449+Mack69@users.noreply.github.com>
2023-03-28 04:28:45 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String args[])
     {
         int a =15;
-        int b =35;
+        int b =45;
         int c =b-a;
         System.out.println(a+b);
         System.out.println(c);
