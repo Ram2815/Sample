eedd264512d64a675d64619f9a72f9c5466b4c30
MAYUKH GHOSH <93669449+Mack69@users.noreply.github.com>
2023-03-28 14:42:12 +0530

    Adding file for exam

---
+++ PracticalExam.java
@@ -0,0 +1,12 @@
+public class PracticalExam
+{
+    public static void main(String args[])
+    {
+        int a= 100;
+        int b= 50;
+        int c= a=b;
+        int d =a-b;
+        System.out.println("Sum ="+ c);
+        System.out.println("Difference ="+ d);
+    }
+}
