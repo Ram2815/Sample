03ebf25cfe7ce2542fb51e5d98a7210ee62232ea
ApoorvSharma14 <apoorv03sharma@gmail.com>
2023-03-28 08:41:01 +0530

    First c lang File

---
+++ sumnatural.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0,n;
+	printf("Enter the natural numbers");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		sum+=i;
+	p-rintf("The sumk of n natural numbers is %d",sum);
+}
+
