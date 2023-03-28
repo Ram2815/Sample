8119a1a54914f58984a5192908288d87bdfe811b
Jeffin03 <114830279+Jeffin03@users.noreply.github.com>
2023-03-28 14:08:42 +0530

    Update div.py

--- div.py
+++ div.py
@@ -1,8 +1,9 @@
 print("This file consist of code that performs division of two numbers")
 print("the variables a and b holds a value each")
 print("while the variable c holds the result of division of a and b")
-a = 58
+a = 90
 b = 65
 c = a/b
 print(" the result of division of a and b is ", c)
+
 
