6f0d4bd30a8eb367d68b9608861bd1b479531c95
Jeffin03 <jeffyissac2203@gmail.com>
2023-03-28 08:39:54 +0530

    commit

--- div.py
+++ div.py
@@ -2,7 +2,7 @@ print("This file consist of code that performs division of two numbers")
 print("the variables a and b holds a value each")
 print("while the variable c holds the result of division of a and b")
 a = 90
-b = 65
+b = 45
 c = a/b
 print(" the result of division of a and b is ", c)
 
